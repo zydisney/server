a8bd209802a23e92cc4b8de9ee4636fb3fd5cef8
Joas Schilling <coding@schilljs.com>
2020-08-28 08:57:06 +0200

    Don't use SELECT DISTINCT when to_char() is used in a WHERE statement
    Nice bug in Oracle
    
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/workflowengine/lib/Manager.php
+++ apps/workflowengine/lib/Manager.php
@@ -147,10 +147,10 @@ class Manager implements IManager {
 	public function getAllConfiguredEvents() {
 		$query = $this->connection->getQueryBuilder();
 
-		$query->selectDistinct('class')
-			->addSelect('entity', 'events')
+		$query->select('class', 'entity', 'events')
 			->from('flow_operations')
-			->where($query->expr()->neq('events', $query->createNamedParameter('[]'), IQueryBuilder::PARAM_STR));
+			->where($query->expr()->neq('events', $query->createNamedParameter('[]'), IQueryBuilder::PARAM_STR))
+			->groupBy('class', 'entity', 'events');
 
 		$result = $query->execute();
 		$operations = [];

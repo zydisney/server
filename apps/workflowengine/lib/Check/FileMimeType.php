a41d98eb7a5bc29e37fe497deb953681434b5246
Robin Appelman <robin@icewind.nl>
2021-02-23 17:32:23 +0100

    use mimetype from cache for workflow if available
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- apps/workflowengine/lib/Check/FileMimeType.php
+++ apps/workflowengine/lib/Check/FileMimeType.php
@@ -21,6 +21,7 @@
 
 namespace OCA\WorkflowEngine\Check;
 
+use OC\Files\Storage\Local;
 use OCA\WorkflowEngine\Entity\File;
 use OCP\Files\IMimeTypeDetector;
 use OCP\Files\Storage\IStorage;
@@ -117,12 +118,12 @@ class FileMimeType extends AbstractStringCheck implements IFileCheck {
 		if ($this->mimeType[$this->storage->getId()][$this->path] !== null) {
 			return $this->mimeType[$this->storage->getId()][$this->path];
 		}
-
-		if ($this->storage->is_dir($this->path)) {
-			return $this->cacheAndReturnMimeType($this->storage->getId(), $this->path, 'httpd/unix-directory');
+		$cacheEntry = $this->storage->getCache()->get($this->path);
+		if ($cacheEntry && $cacheEntry->getMimeType() !== 'application/octet-stream') {
+			return $this->cacheAndReturnMimeType($this->storage->getId(), $this->path, $cacheEntry->getMimeType());
 		}
 
-		if ($this->storage->file_exists($this->path)) {
+		if ($this->storage->file_exists($this->path) && $this->storage->instanceOfStorage(Local::class)) {
 			$path = $this->storage->getLocalFile($this->path);
 			$mimeType = $this->mimeTypeDetector->detectContent($path);
 			return $this->cacheAndReturnMimeType($this->storage->getId(), $this->path, $mimeType);
